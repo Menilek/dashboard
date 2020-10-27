d1820fabaa36ea4d794a0bb679961c960c480a73
Menilek Techane <menilek@synaptek.ninja>
2020-10-27 19:09:19 +0000

    Created Dockerfile and added simple styling

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3
+
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+
+RUN pip install --user --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 5000
+
+CMD ["python", "./app.py"]
+
+# docker build --tag mysite . 
+# docker run --publish 5050:5000 --detach --name bb mysite
+# docker stop bb
+# docker rm bb
--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    header = 'Menilek.Tech/ane'
     title = 'HELLO WORLD'
-    return render_template('index.html', title=title)
+    return render_template('index.html', header=header, title=title)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
