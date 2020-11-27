5471c2e4f63dc72bf7c396ce4f3d9b92bd081632
Menilek Techane <menilek@synaptek.ninja>
2020-11-27 16:02:01 +0000

    Implemented flask-cors and updated reqirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_cors import CORS, cross_origin
 from api import api
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 app.register_blueprint(api)
 
 @app.route('/', methods=['GET'])
+@cross_origin(supports_credentials=True)
 def home():
     return redirect(url_for('ane.index'))
 
