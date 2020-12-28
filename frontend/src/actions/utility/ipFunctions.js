d086db69bf4ac79cdce57c0d1982ea9eb8686774
Menilek Techane <menilek@synaptek.ninja>
2020-12-28 10:48:05 +0000

    Moved api call to the client to resolve api response issue

--- frontend/src/actions/utility/ipFunctions.js
+++ frontend/src/actions/utility/ipFunctions.js
@@ -2,16 +2,18 @@ import axios from "axios";
 
 export const getIPData = async () => {
   try {
-    let endpoint = process.env.REACT_APP_BACKEND_URL;
+    // let endpoint = process.env.REACT_APP_BACKEND_URL;
+    // const res = await axios.get("http://" + endpoint + ":5000/api/visitor");
 
-    const res = await axios.get("http://" + endpoint + ":5000/api/visitor");
+    const res = await axios.get("http://ip-api.com/json/");
 
     let ipData = {
       city: res.data.city,
       isp: res.data.isp,
-      ip: res.data.ip,
-      tz: res.data.tz,
+      ip: res.data.query,
+      tz: res.data.timezone,
     };
+
     return ipData;
   } catch (err) {
     console.error(err);
