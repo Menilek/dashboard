bc585fd2b7a66220cba2fe473e0be5549f535045
Menilek <menilek25@hotmail.co.uk>
2021-01-12 23:23:55 +0000

    Removed redundant routes

--- backend/api.py
+++ backend/api.py
@@ -37,51 +37,6 @@ def visit():
 # }
 
 
-@api.route('/api/visitor', methods=['GET'])
-def visitor():
-    ip_endpoint = 'http://ip-api.com/json/'
-    ip_data = requests.get(ip_endpoint)
-    ip_payload = ip_data.json()
-    city = ip_payload['city']
-    isp = ip_payload['isp']
-    ip = ip_payload['query']
-    tz = ip_payload['timezone']
-    ip_obj = {"city": city, "isp": isp, "ip": ip, "tz": tz}
-    return ip_obj
-
-
-@api.route('/api/city', methods=['GET'])
-def city():
-    endpoint = 'http://ip-api.com/json/?fields=city'
-    city_data = requests.get(endpoint)
-    city = city_data.json()
-    return city
-
-
-@api.route('/api/ip', methods=['GET'])
-def ip_address():
-    endpoint = 'http://ip-api.com/json/?fields=query'
-    ip_data = requests.get(endpoint)
-    ip_address = ip_data.json()
-    return ip_address
-
-
-@api.route('/api/isp', methods=['GET'])
-def isp():
-    endpoint = 'http://ip-api.com/json/?fields=isp'
-    isp_data = requests.get(endpoint)
-    isp = isp_data.json()
-    return isp
-
-
-@api.route('/api/timezone', methods=['GET'])
-def timezone():
-    endpoint = 'http://ip-api.com/json/?fields=timezone'
-    timezone_data = requests.get(endpoint)
-    timezone = timezone_data.json()
-    return timezone
-
-
 @api.route('/api/docs', methods=['GET'])
 def get_docs():
     print('Retrieving docs')
--- backend/app.py
+++ backend/app.py
@@ -6,18 +6,7 @@ from flask import Flask, render_template, redirect, url_for
 
 app.register_blueprint(api)
 
-@app.route('/', methods=['GET'])
 @cross_origin(supports_credentials=True)
-def home():
-    db_data = getEntries()
-    print(db_data)
-    response = app.response_class(
-        response=json.dumps(db_data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
