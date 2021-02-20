aa4cae28411a458c77c6d1960f97fb03e308519b
Menilek <menilek25@hotmail.co.uk>
2021-02-20 00:48:58 +0000

    Added Rows and Cols

--- frontend/src/components/splash.js
+++ frontend/src/components/splash.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Row, Col } from "reactstrap";
 import "../App.css";
 import dashboard from "../img/dashboard.png";
 import cafesite from "../img/cafe-site.png";
@@ -8,51 +9,68 @@ import p4p from "../img/p4p.png";
 // import fantasyfighter from "../img/fantasyfighter.png";
 
 const Container = styled.div`
-  font-size: 2em;
-  text-align: right;
-  font-family: sans-serif;
+  // font-size: 2em;
+  // text-align: right;
+  // font-family: sans-serif;
 `;
 
 function Splash() {
   return (
     <div>
-      <p className="big-title floating">
-        &#123;&#123; Menilek.tech/ane &#125;&#125;
-      </p>
       <div className="grid">
-        <div className="cell">
-          <Container>
-            <a href="http://www.gillianscafe.co.uk/">
-              <i class="fas fa-utensils"></i>
-            </a>
-          </Container>
-          <a href="http://www.gillianscafe.co.uk/">
-            <img src={cafesite} className="site-thumb" alt="site thumbnail" />
-          </a>
-        </div>
-        <div className="cell">
-          <Container>
-            <a href="https://github.com/Menilek/ip-dashboard">
-              <i class="fab fa-github"></i>
-            </a>
-          </Container>
-          <Link to="/ane/dashboard">
-            <img src={dashboard} className="site-thumb" alt="site thumbnail" />
-          </Link>
-          {/* <a href="http://menilek.tech/ane/dashboard">
-            <img src={dashboard} className="site-thumb" alt="site thumbnail" />
-          </a> */}
-        </div>
-        <div className="cell">
-          <Container>
-            <a href="https://github.com/Menilek/PoundForPound">
-              <i class="fab fa-github"></i>
-            </a>
-          </Container>
-          <a href="https://top-fighters.herokuapp.com/">
-            <img src={p4p} className="site-thumb" alt="site thumbnail" />
-          </a>
-        </div>
+      <Row>
+        <Col sm={12} lg={12}>
+          <h1 className="big-title floating">
+            &#123;&#123; menilek.tech/ane &#125;&#125;
+          </h1>
+        </Col>
+      </Row>
+        <Row>
+          <Col sm={12} lg={4}>
+            <div className="cell">
+              <Container>
+                <a href="http://www.gillianscafe.co.uk/">
+                  <i class="fas fa-utensils"></i>
+                </a>
+              </Container>
+              <a href="http://www.gillianscafe.co.uk/">
+                <img
+                  src={cafesite}
+                  className="site-thumb"
+                  alt="site thumbnail"
+                />
+              </a>
+            </div>
+          </Col>
+          <Col sm={12} lg={4}>
+            <div className="cell">
+              <Container>
+                <a href="https://github.com/Menilek/ip-dashboard">
+                  <i class="fab fa-github"></i>
+                </a>
+              </Container>
+              <Link to="/ane/dashboard">
+                <img
+                  src={dashboard}
+                  className="site-thumb"
+                  alt="site thumbnail"
+                />
+              </Link>
+            </div>
+          </Col>
+          <Col sm={12} lg={4}>
+            <div className="cell">
+              <Container>
+                <a href="https://github.com/Menilek/PoundForPound">
+                  <i class="fab fa-github"></i>
+                </a>
+              </Container>
+              <a href="https://top-fighters.herokuapp.com/">
+                <img src={p4p} className="site-thumb" alt="site thumbnail" />
+              </a>
+            </div>
+          </Col>
+        </Row>
         {/* <div className="cell">
           <Container>
             <a href="https://github.com/Menilek/FantasyFighter">
