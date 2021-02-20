8e583bfad04593731e68df2d020943329e49d845
Menilek <menilek25@hotmail.co.uk>
2021-02-20 00:46:46 +0000

    Extracted theme colours from App.js

---
+++ frontend/src/components/themecolours.js
@@ -0,0 +1,11 @@
+import theme from "styled-theming";
+
+export const backgroundColour = theme("theme", {
+    light: "#fff",
+    dark: "#000",
+  });
+  
+  export const textColour = theme("theme", {
+    light: "#000",
+    dark: "#fff",
+  });
