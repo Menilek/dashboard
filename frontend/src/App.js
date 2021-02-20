39a7b56e5924fc85f56e626bbcc5467d6bf44e2c
Menilek <menilek25@hotmail.co.uk>
2021-02-20 00:50:33 +0000

    Tidied App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import { hot } from "react-hot-loader/root";
 import {
   BrowserRouter as Router,
@@ -7,61 +7,18 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-import styled from "styled-components";
-import theme from "styled-theming";
-import store from "./store";
-import DarkThemeProvider from "./DarkThemeProvider";
-import DarkThemeToggle from "./DarkThemeToggle";
-import IpDashboard from "./components/dashboard.js";
-import Splash from "./components/splash";
-// import NavBar from "./components/navbar";
-
-export const backgroundColour = theme("theme", {
-  light: "#fff",
-  dark: "#000",
-});
-
-export const textColour = theme("theme", {
-  light: "#000",
-  dark: "#fff",
-});
-
-const Container = styled.div`
-  // display: flex;
-  // flex-direction: column;
-  width: 100vw;
-  height: 100vh;
-  // align-items: center;
-  // justify-content: center;
-  // font-family: sans-serif;
-  background-color: ${backgroundColour};
-  color: ${textColour};
-`;
+import SplashPage from "./pages/splashPage";
+import DashboardPage from "./pages/dashboardPage";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/ane/dashboard">
-          <Provider store={store}>
-            <DarkThemeProvider>
-              <Container>
-                {/* <NavBar /> */}
-                <DarkThemeToggle />
-                <IpDashboard />
-              </Container>
-            </DarkThemeProvider>
-          </Provider>
+          <DashboardPage />
         </Route>
         <Route path="/ane">
-          <Provider store={store}>
-            <DarkThemeProvider>
-              <Container>
-                <DarkThemeToggle />
-                <Splash />
-              </Container>
-            </DarkThemeProvider>
-          </Provider>
+          <SplashPage />
         </Route>
         <Route path="/">
           <Redirect to="/ane" />
