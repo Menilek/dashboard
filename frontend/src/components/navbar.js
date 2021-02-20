4ad2320e49794c162b2624ce9849049793b87592
Menilek <menilek25@hotmail.co.uk>
2021-02-20 15:47:29 +0000

    Moved navbar icons

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -5,14 +5,12 @@ import "../App.css";
 function NavBar() {
   return (
     <div>
-      <Navbar  light expand="lg">
-        <NavLink href="/ane/dashboard">
-          <i class="fas fa-wifi"></i>
-        </NavLink>
-      </Navbar>
-      <Navbar  light expand="lg">
+      <Navbar expand="lg">
         <NavLink href="/ane">
           <i class="fas fa-home"></i>
+        </NavLink>
+        <NavLink href="/ane/dashboard">
+          <i class="fas fa-wifi"></i>
         </NavLink>
       </Navbar>
     </div>
