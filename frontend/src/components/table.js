d5c12b4d0bc7ada07c2367f31a0e80beb0ecdf2e
Menilek <menilek25@hotmail.co.uk>
2021-03-21 23:29:24 +0000

    Added modal to create new words and delete functionality to remove a word

--- frontend/src/components/table.js
+++ frontend/src/components/table.js
@@ -1,8 +1,11 @@
 import { React, useState, useEffect } from "react";
 import { Table } from "reactstrap";
-import { getWords } from "../actions/utility/dbActions";
+import WordModal from "./wordModal";
+import Loader from "./loader";
+import { getWords, deleteWord } from "../actions/utility/dbActions";
 
 const LanguageTable = () => {
+  const [loading, setLoading] = useState(true);
   const [words, setWords] = useState([]);
 
   const fetchWords = async () => {
@@ -10,40 +13,64 @@ const LanguageTable = () => {
     setWords(res);
   };
 
+  const dropWord = async (e) => {
+    const id = e.target.getAttribute("name");
+    setWords(words.filter((item) => item._id !== id));
+    await deleteWord(id);
+  };
+
   useEffect(() => {
     try {
       fetchWords();
+      setLoading(false);
     } catch (err) {
       console.error(err);
     }
   }, []);
 
-  return (
-    <div>
-      <h1 className="languageTitle">አማርኛ መማር</h1>
-      <div className="languageTable">
-        <Table>
-          <thead>
-            <tr>
-              <th>English</th>
-              <th>āmarinya</th>
-              <th>Ge'ez</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {words?.map((word) => (
-              <tr key={word.english}>
-                <td>{word.english}</td>
-                <td>{word.amharic}</td>
-                <td>{word.geez}</td>
+  if (loading) {
+    return <Loader />;
+  } else {
+    return (
+      <div>
+        <h1 className="languageTitle">አማርኛ መማር</h1>
+        <WordModal />
+        <div className="languageTable">
+          <Table>
+            <thead>
+              <tr>
+                <th>English</th>
+                <th>āmarinya</th>
+                <th>Ge'ez</th>
+                <th>Edit</th>
+                <th>Delete</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+
+            <tbody>
+              {words?.map((word) => (
+                <tr key={word._id}>
+                  <td>{word.english}</td>
+                  <td>{word.amharic}</td>
+                  <td>{word.geez}</td>
+                  <td>
+                    <i className="fas fa-edit tableAction"></i>
+                  </td>
+                  <td>
+                    <i
+                      name={word._id}
+                      onClick={dropWord}
+                      className="fas fa-trash-alt tableAction"
+                    ></i>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default LanguageTable;
