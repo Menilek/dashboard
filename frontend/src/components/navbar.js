b76c944958bd84e1e6c465bf36e34fd500108ce5
Menilek <menilek25@hotmail.co.uk>
2021-03-21 01:28:29 +0000

    Installed concurrently and tidied files

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -7,10 +7,10 @@ function NavBar() {
     <div>
       <Navbar expand="sm">
         <NavLink href="/ane">
-          <i class="fas fa-home"></i>
+          <i className="fas fa-home"></i>
         </NavLink>
         <NavLink href="/ane/dashboard">
-          <i class="fas fa-wifi"></i>
+          <i className="fas fa-wifi"></i>
         </NavLink>
         <NavLink href="/ane/learn-amharic">
           <i className="amharic-link">አማርኛ</i>
