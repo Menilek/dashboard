980ec2d80ed81e7a9f20041d8bb699818dc51c04
Menilek <menilek25@hotmail.co.uk>
2021-03-21 23:19:56 +0000

    Added seed and empty database functions

--- backend/AmharicAPI/server.js
+++ backend/AmharicAPI/server.js
@@ -4,13 +4,12 @@ const cors = require("cors");
 const path = require("path");
 const dotenv = require("dotenv");
 const words = require("./routes/api/words");
-// const seedDatabase = require("./seeds/seedDatabase");
+// const { seedDatabase, emptyDatabase } = require("./scripts/db");
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 
 const url = process.env.MONGODB_URI;
 
@@ -26,6 +25,7 @@ mongoose
 app.use("/api/words", words);
 
 // seedDatabase();
+// emptyDatabase();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../../frontend/build"));
