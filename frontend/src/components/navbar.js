de1d855aeae751b822053aa0faea67204c40b262
Menilek <menilek25@hotmail.co.uk>
2021-04-27 17:41:27 +0100

    Mapping through navbar items

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,18 +3,33 @@ import { Navbar, NavLink } from "reactstrap";
 import "../App.css";
 
 function NavBar() {
+  const items = [
+    {
+      path: "/ane",
+      icon: "fas fa-home",
+      value: 'home'
+    },
+    {
+      path: "/ane/dashboard",
+      icon: "fas fa-wifi",
+      value: 'wifi'
+    },
+    {
+      path: "/ane/learn-amharic",
+      icon: "amharic-link",
+      value: 'amharic',
+      text: "አማርኛ",
+    },
+  ];
+
   return (
     <div>
       <Navbar expand="sm">
-        <NavLink href="/ane">
-          <i className="fas fa-home"></i>
-        </NavLink>
-        <NavLink href="/ane/dashboard">
-          <i className="fas fa-wifi"></i>
-        </NavLink>
-        <NavLink href="/ane/learn-amharic">
-          <i className="amharic-link">አማርኛ</i>
-        </NavLink>
+        {items.map((item) => (
+          <NavLink href={item.path} key={item.value}>
+            <i className={item.icon}>{item.text}</i>
+          </NavLink>
+        ))}
       </Navbar>
     </div>
   );
