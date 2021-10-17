121049040b31bbaeab045e582ed5ac80bf94416b
Menilek Techane <menilek.techane@ultromics.com>
2021-10-17 14:22:25 +0100

    Extract out category options

---
+++ client/src/util/CategoryOptions.js
@@ -0,0 +1,46 @@
+export const categoryOptions = [
+    {
+      category: "NOUN - a word that names something",
+      value: "Noun",
+      key: "noun",
+    },
+    {
+      category: "VERB - describes an action, state or occurrence",
+      value: "Verb",
+      key: "verb",
+    },
+    {
+      category:
+        "ADVERB - modifies/describes a verb/adjective/or another adverb",
+      value: "Adverb",
+      key: "adverb",
+    },
+    {
+      category: "ADJECTIVE - describes a noun",
+      value: "Adjective",
+      key: "adjective",
+    },
+    {
+      category:
+        "PREPOSITION - shows direction, time, place, location, spatial relationship",
+      value: "Preposition",
+      key: "preposition",
+    },
+    { category: "PHRASE", value: "Phrase", key: "phrase" },
+    {
+      category: "CONJUNCTION - connects words, phrases, clauses or sentences",
+      value: "Conjunction",
+      key: "conjunction",
+    },
+    {
+      category: "PRONOUN - takes the place of a noun e.g: he/they/hers",
+      value: "Pronoun",
+      key: "pronoun",
+    },
+    {
+      category: "SLANG - informal words/phrases",
+      value: "Slang",
+      key: "slang",
+    },
+    { category: "UNKNOWN", value: "Unknown", key: "unknown" },
+  ];
