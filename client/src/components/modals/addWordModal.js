896804ce8f6ad057f74e699ff12871329893324f
Menilek <Menilek25@hotmail.co.uk>
2021-11-15 11:34:01 +0000

    Update AddWordModal

--- client/src/components/modals/addWordModal.js
+++ client/src/components/modals/addWordModal.js
@@ -1,141 +1,75 @@
-import { React, useState } from "react";
-import {
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Col,
-} from "reactstrap";
-import { addWord } from "../../actions/utility/dbActions";
+import { React } from 'react';
+import { FormGroup, Label, Input, Col } from 'reactstrap';
+import { categoryOptions } from '../../util/CategoryOptions';
 
-const AddWordModal = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const [english, setEnglish] = useState("");
-  const [amharic, setAmharic] = useState("");
-  const [geez, setGeez] = useState("");
-  const [category, setCategory] = useState("verb");
-
-  const toggleModal = () => {
-    setModalOpen(!modalOpen);
-  };
-
-  const clearState = () => {
-    setEnglish("");
-    setAmharic("");
-    setGeez("");
-    setCategory("verb");
-  };
-
-  //TODO: ADD VALIDATION: no numbers, only geez can be empty
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const word = { english, amharic, geez, category };
-      await addWord(word);
-      console.log(category);
-      clearState();
-      toggleModal();
-      //RE-RENDER TABLE
-    } catch (err) {
-      console.error(err);
-      clearState();
-      toggleModal();
-    }
-  };
-
+const AddWordModal = ({
+  english,
+  setEnglish,
+  amharic,
+  setAmharic,
+  geez,
+  setGeez,
+  category,
+  setCategory
+}) => {
   return (
     <div>
-      <div className="languageButton">
-        <Button color="primary" onClick={toggleModal}>
-          Add Word
-        </Button>
-      </div>
-      <Modal
-        contentClassName="languageModal"
-        isOpen={modalOpen}
-        toggle={toggleModal}
-      >
-        <ModalHeader toggle={toggleModal}>Add a Word</ModalHeader>
-        <ModalBody>
-          <Form onSubmit={handleSubmit}>
-            <Col sm={12} md={12} lg={12}>
-              <FormGroup>
-                <Label for="englishWord">English</Label>
-                <Input
-                  type="text"
-                  name="english"
-                  value={english}
-                  id="englishWord"
-                  placeholder="Amharic"
-                  onChange={(e) => setEnglish(e.target.value)}
-                />
-              </FormGroup>
-            </Col>
-            <Col sm={12} md={12} lg={12}>
-              <FormGroup>
-                <Label for="amharichWord">Amharic</Label>
-                <Input
-                  type="text"
-                  name="amharic"
-                  value={amharic}
-                  id="amharichWord"
-                  placeholder="āmarinya"
-                  onChange={(e) => setAmharic(e.target.value)}
-                />
-              </FormGroup>
-            </Col>
-            <Col sm={12} md={12} lg={12}>
-              <FormGroup>
-                <Label for="geezWord">Ge'ez</Label>
-                <Input
-                  type="text"
-                  name="geez"
-                  value={geez}
-                  id="geezWord"
-                  placeholder="አማርኛ"
-                  onChange={(e) => setGeez(e.target.value)}
-                />
-              </FormGroup>
-            </Col>
-            <Col sm={12} md={12} lg={12}>
-              <FormGroup>
-                <Label for="wordCategory">Category</Label>
-                <Input
-                  type="select"
-                  name="category"
-                  id="wordCategory"
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value)}
-                >
-                  <option value="verb" key="verb">
-                    Verb
-                  </option>
-                  <option value="adverb" key="adverb">
-                    Adverb
-                  </option>
-                  <option value="adjective" key="adjective">
-                    Adjective
-                  </option>
-                  <option value="slang" key="slang">
-                    Slang
-                  </option>
-                  <option value="other" key="other">
-                    Other/Unknown
-                  </option>
-                </Input>
-              </FormGroup>
-            </Col>
-            <Button block color="primary" onClick={handleSubmit}>
-              ADD
-            </Button>
-          </Form>
-        </ModalBody>
-      </Modal>
+      <Col sm={12} md={12} lg={12}>
+        <FormGroup>
+          <Label for="englishWord">English</Label>
+          <Input
+            type="text"
+            name="english"
+            value={english}
+            id="englishWord"
+            placeholder="Amharic"
+            onChange={(e) => setEnglish(e.target.value)}
+          />
+        </FormGroup>
+      </Col>
+      <Col sm={12} md={12} lg={12}>
+        <FormGroup>
+          <Label for="amharichWord">Amharic</Label>
+          <Input
+            type="text"
+            name="amharic"
+            value={amharic}
+            id="amharichWord"
+            placeholder="āmarinya"
+            onChange={(e) => setAmharic(e.target.value)}
+          />
+        </FormGroup>
+      </Col>
+      <Col sm={12} md={12} lg={12}>
+        <FormGroup>
+          <Label for="geezWord">Ge&apos;ez</Label>
+          <Input
+            type="text"
+            name="geez"
+            value={geez}
+            id="geezWord"
+            placeholder="አማርኛ"
+            onChange={(e) => setGeez(e.target.value)}
+          />
+        </FormGroup>
+      </Col>
+      <Col sm={12} md={12} lg={12}>
+        <FormGroup>
+          <Label for="wordCategory">Category</Label>
+          <Input
+            type="select"
+            name="category"
+            id="wordCategory"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}>
+            {categoryOptions.map((category) => (
+              <option value={category.value} key={category.key}>
+                {category.category}
+              </option>
+            ))}
+          </Input>
+        </FormGroup>
+      </Col>
     </div>
   );
 };
