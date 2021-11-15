e61fd3ee9b26b493011269df1ff78be159a93570
Menilek <Menilek25@hotmail.co.uk>
2021-11-15 11:36:16 +0000

    Implement Favourites feature and add Modals

--- client/src/components/wordTable.js
+++ client/src/components/wordTable.js
@@ -1,51 +1,60 @@
-import { React, useState, useEffect } from "react";
-import {
-  Row,
-  Col,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Table,
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-} from "reactstrap";
+import React, { useState, useEffect } from 'react';
+import { Row, Form, Table, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { reject } from 'lodash';
 // import CategoryDropdown from "./dropDown";
-import Loader from "./loader";
-import CategoryBadge from "./categoryBadge";
-import { categoryOptions } from "../util/CategoryOptions";
-import { getWords, deleteWord, addWord } from "../actions/utility/dbActions";
+import Loader from './loader';
+import CategoryBadge from './categoryBadge';
+import FavouriteModal from './modals/favouriteModal';
+import AddWordModal from './modals/addWordModal';
+import { getWords, deleteWord, addWord } from '../actions/utility/dbActions';
+import EditWordModal from './modals/editWordModal';
 
 const LanguageTable = () => {
   const [loading, setLoading] = useState(true);
   const [words, setWords] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+  const [favouritesModalOpen, setFavouritesModalOpen] = useState(false);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
-  const [modalWord, setModalWord] = useState("");
-  const [modalID, setModalID] = useState("");
-  const [editModalWord, setEditModalWord] = useState("");
+  const [modalWord, setModalWord] = useState('');
+  const [modalID, setModalID] = useState('');
+  const [editModalWord, setEditModalWord] = useState('');
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [addModalOpen, setAddModalOpen] = useState(false);
-  const [english, setEnglish] = useState("");
-  const [amharic, setAmharic] = useState("");
-  const [geez, setGeez] = useState("");
-  const [category, setCategory] = useState("Verb");
-  const [query, setQuery] = useState("");
-  const [searchParam] = useState(["english", "amharic", "geez"]);
+  const [english, setEnglish] = useState('');
+  const [amharic, setAmharic] = useState('');
+  const [geez, setGeez] = useState('');
+  const [category, setCategory] = useState('Verb');
+  const [query, setQuery] = useState('');
+  const [searchParam] = useState(['english', 'amharic', 'geez']);
+
+  const toggleFavourite = (e) => {
+    const amharic = e.target.getAttribute('value');
+    const english = e.target.getAttribute('name');
+    const word = { english: english, amharic: amharic };
+    if (Array.isArray(favourites) && !favourites.length) {
+      setFavourites((favourites) => favourites.concat(word));
+      const favouritesString = JSON.stringify(favourites);
+      localStorage.setItem('Favourites', favouritesString);
+    } else {
+      let newFaves = reject(favourites, (fave) => fave['amharic'] === word['amharic']);
+      setFavourites(newFaves);
+      const favouritesString = JSON.stringify(favourites);
+      localStorage.setItem('Favourites', favouritesString);
+    }
+  };
 
   const toggleDeleteModal = (e) => {
-    const deleteWord = e.target.getAttribute("value");
+    const deleteWord = e.target.getAttribute('value');
+    const id = e.target.getAttribute('name');
     setModalWord(deleteWord);
-    const id = e.target.getAttribute("name");
     setModalID(id);
     setDeleteModalOpen(!deleteModalOpen);
   };
 
   const toggleEditModal = (e) => {
-    const amharicWord = e.target.getAttribute("value");
+    const amharicWord = e.target.getAttribute('value');
     setModalWord(amharicWord);
-    const englishWord = e.target.getAttribute("name");
+    const englishWord = e.target.getAttribute('name');
     setEditModalWord(englishWord);
     setEditModalOpen(!editModalOpen);
   };
@@ -54,11 +63,15 @@ const LanguageTable = () => {
     setAddModalOpen(!addModalOpen);
   };
 
+  const toggleFavouritesModal = () => {
+    setFavouritesModalOpen(!favouritesModalOpen);
+  };
+
   const clearState = () => {
-    setEnglish("");
-    setAmharic("");
-    setGeez("");
-    setCategory("Verb");
+    setEnglish('');
+    setAmharic('');
+    setGeez('');
+    setCategory('Verb');
   };
 
   const fetchWords = async () => {
@@ -66,17 +79,26 @@ const LanguageTable = () => {
     setWords(res);
   };
 
-  const dropWord = async (e) => {
+  const fetchFavourites = () => {
+    let faves = JSON.parse(localStorage.getItem('Favourites'));
+    if (faves === null) {
+      faves = [];
+    }
+    setFavourites(faves);
+  };
+
+  const dropWord = async () => {
     setWords(words.filter((item) => item._id !== modalID));
     await deleteWord(modalID);
-    setModalWord("");
-    setModalID("");
+    setModalWord('');
+    setModalID('');
     setDeleteModalOpen(!deleteModalOpen);
   };
 
-  const editWord = async (e) => {
+  const editWord = async () => {
     // MAKE PATCH REQUEST
-    console.log("TO DO: IMPLEMENT EDIT FUNCTIONALITY");
+    console.log('TO DO: IMPLEMENT EDIT FUNCTIONALITY');
+    setEditModalOpen(!editModalOpen);
   };
 
   const handleSubmit = async (e) => {
@@ -94,18 +116,13 @@ const LanguageTable = () => {
     }
   };
 
-  const search = items => {
+  const search = () => {
     return words.filter((word) => {
       return searchParam.some((newWord) => {
-        return (
-          word[newWord]
-            .toString()
-            .toLowerCase()
-            .indexOf(query.toLowerCase()) > -1
-        );
+        return word[newWord].toString().toLowerCase().indexOf(query.toLowerCase()) > -1;
       });
     });
-  }
+  };
 
   // const filterCategory = (e) => {
   //   let category = e.target.getAttribute("value");
@@ -114,6 +131,7 @@ const LanguageTable = () => {
   useEffect(() => {
     try {
       fetchWords();
+      fetchFavourites();
       setLoading(false);
     } catch (err) {
       console.error(err);
@@ -136,99 +154,56 @@ const LanguageTable = () => {
               value={query}
               onChange={(e) => setQuery(e.target.value)}
             />
-            <div className="languageButton">
+            <div>
               <Button color="primary" onClick={toggleAddModal}>
                 Add Word
               </Button>
+              <Button color="info" onClick={toggleFavouritesModal} id="fave-btn">
+                Browse Favourites
+              </Button>
             </div>
-            {/* <div className="category-bar">
+          </div>
+          {/* <div className="category-bar">
               {categoryOptions.map((category) => (
                 <span className="category-filter" onClick={filterCategory} tabindex="-1">
                   <CategoryBadge category={category.value} />
                 </span>
               ))}
             </div> */}
-            <Modal
-              contentClassName="languageModal"
-              isOpen={addModalOpen}
-              toggle={toggleAddModal}
-            >
-              <ModalHeader toggle={toggleAddModal}>Add a Word</ModalHeader>
-              <ModalBody>
-                <Form onSubmit={handleSubmit}>
-                  <Col sm={12} md={12} lg={12}>
-                    <FormGroup>
-                      <Label for="englishWord">English</Label>
-                      <Input
-                        type="text"
-                        name="english"
-                        value={english}
-                        id="englishWord"
-                        placeholder="Amharic"
-                        onChange={(e) => setEnglish(e.target.value)}
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col sm={12} md={12} lg={12}>
-                    <FormGroup>
-                      <Label for="amharichWord">Amharic</Label>
-                      <Input
-                        type="text"
-                        name="amharic"
-                        value={amharic}
-                        id="amharichWord"
-                        placeholder="āmarinya"
-                        onChange={(e) => setAmharic(e.target.value)}
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col sm={12} md={12} lg={12}>
-                    <FormGroup>
-                      <Label for="geezWord">Ge'ez</Label>
-                      <Input
-                        type="text"
-                        name="geez"
-                        value={geez}
-                        id="geezWord"
-                        placeholder="አማርኛ"
-                        onChange={(e) => setGeez(e.target.value)}
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col sm={12} md={12} lg={12}>
-                    <FormGroup>
-                      <Label for="wordCategory">Category</Label>
-                      <Input
-                        type="select"
-                        name="category"
-                        id="wordCategory"
-                        value={category}
-                        onChange={(e) => setCategory(e.target.value)}
-                      >
-                        {categoryOptions.map((category) => (
-                          <option value={category.value} key={category.key}>
-                            {category.category}
-                          </option>
-                        ))}
-                      </Input>
-                    </FormGroup>
-                  </Col>
-                  <Button block color="primary" onClick={handleSubmit}>
-                    ADD
-                  </Button>
-                </Form>
-              </ModalBody>
-            </Modal>
-          </div>
 
           <Modal
-            contentClassName="deleteModal"
-            isOpen={deleteModalOpen}
-            toggle={toggleDeleteModal}
-          >
-            <ModalHeader toggle={toggleDeleteModal}>
-              Delete: {modalWord}?
-            </ModalHeader>
+            contentClassName="favouritesModal"
+            isOpen={favouritesModalOpen}
+            toggle={toggleFavouritesModal}>
+            <ModalHeader toggle={toggleFavouritesModal}>Here are your faves!</ModalHeader>
+            <ModalBody>
+              <FavouriteModal favourites={favourites} />
+            </ModalBody>
+          </Modal>
+
+          <Modal contentClassName="languageModal" isOpen={addModalOpen} toggle={toggleAddModal}>
+            <ModalHeader toggle={toggleAddModal}>Add a Word</ModalHeader>
+            <ModalBody>
+              <Form onSubmit={handleSubmit}>
+                <AddWordModal
+                  english={english}
+                  setEnglish={setEnglish}
+                  amharic={amharic}
+                  setAmharic={setAmharic}
+                  geez={geez}
+                  setGeez={setGeez}
+                  category={category}
+                  setCategory={setCategory}
+                />
+                <Button block color="primary" onClick={handleSubmit}>
+                  ADD
+                </Button>
+              </Form>
+            </ModalBody>
+          </Modal>
+
+          <Modal contentClassName="deleteModal" isOpen={deleteModalOpen} toggle={toggleDeleteModal}>
+            <ModalHeader toggle={toggleDeleteModal}>Delete: {modalWord}?</ModalHeader>
             <ModalBody>
               <Row className="deleteButtons">
                 <Button onClick={dropWord} color="danger">
@@ -241,73 +216,20 @@ const LanguageTable = () => {
             </ModalBody>
           </Modal>
 
-          <Modal
-            contentClassName="editModal"
-            isOpen={editModalOpen}
-            toggle={toggleEditModal}
-          >
-            <ModalHeader toggle={toggleEditModal}>
-              Edit: {modalWord}
-            </ModalHeader>
+          <Modal contentClassName="editModal" isOpen={editModalOpen} toggle={toggleEditModal}>
+            <ModalHeader toggle={toggleEditModal}>Edit: {editModalWord}</ModalHeader>
             <ModalBody>
               <Form onSubmit={editWord}>
-                <Col sm={12} md={12} lg={12}>
-                  <FormGroup>
-                    <Label for="englishWord">English</Label>
-                    <Input
-                      type="text"
-                      name="english"
-                      // value={english}
-                      id="englishWord"
-                      placeholder="Amharic"
-                      // onChange={(e) => setEnglish(e.target.value)}
-                    />
-                  </FormGroup>
-                </Col>
-                <Col sm={12} md={12} lg={12}>
-                  <FormGroup>
-                    <Label for="amharichWord">Amharic</Label>
-                    <Input
-                      type="text"
-                      name="amharic"
-                      // value={amharic}
-                      id="amharichWord"
-                      placeholder="āmarinya"
-                      // onChange={(e) => setAmharic(e.target.value)}
-                    />
-                  </FormGroup>
-                </Col>
-                <Col sm={12} md={12} lg={12}>
-                  <FormGroup>
-                    <Label for="geezWord">Ge'ez</Label>
-                    <Input
-                      type="text"
-                      name="geez"
-                      // value={geez}
-                      id="geezWord"
-                      placeholder="አማርኛ"
-                      // onChange={(e) => setGeez(e.target.value)}
-                    />
-                  </FormGroup>
-                </Col>
-                <Col sm={12} md={12} lg={12}>
-                  <FormGroup>
-                    <Label for="wordCategory">Category</Label>
-                    <Input
-                      type="select"
-                      name="category"
-                      id="wordCategory"
-                      // value={category}
-                      // onChange={(e) => setCategory(e.target.value)}
-                    >
-                      {categoryOptions.map((category) => (
-                        <option value={category.value} key={category.key}>
-                          {category.category}
-                        </option>
-                      ))}
-                    </Input>
-                  </FormGroup>
-                </Col>
+                <EditWordModal
+                  english={english}
+                  setEnglish={setEnglish}
+                  amharic={amharic}
+                  setAmharic={setAmharic}
+                  geez={geez}
+                  setGeez={setGeez}
+                  category={category}
+                  setCategory={setCategory}
+                />
                 <Button block color="primary" onClick={editWord}>
                   EDIT
                 </Button>
@@ -324,8 +246,9 @@ const LanguageTable = () => {
               <tr>
                 <th>English</th>
                 <th>āmarinya</th>
-                <th>Ge'ez</th>
+                <th>Ge&apos;ez</th>
                 <th>Category</th>
+                <th>Favourite</th>
                 <th>Edit</th>
                 <th>Delete</th>
               </tr>
@@ -344,17 +267,27 @@ const LanguageTable = () => {
                     <i
                       value={word.amharic}
                       name={word.english}
+                      onClick={toggleFavourite}
+                      className={`tableAction ${
+                        favourites &&
+                        favourites.some((favourite) => favourite.amharic === word.amharic)
+                          ? 'fas fa-star'
+                          : 'far fa-star'
+                      }`}></i>
+                  </td>
+                  <td>
+                    <i
+                      value={word.amharic}
+                      name={word.english}
                       onClick={toggleEditModal}
-                      className="fas fa-edit tableAction"
-                    ></i>
+                      className="fas fa-edit tableAction"></i>
                   </td>
                   <td>
                     <i
                       value={word.amharic}
                       name={word._id}
                       onClick={toggleDeleteModal}
-                      className="fas fa-trash-alt tableAction"
-                    ></i>
+                      className="fas fa-trash-alt tableAction"></i>
                   </td>
                 </tr>
               ))}
